e1192f02a78a7aef3135818a6f5625674ff07b22
Veronika <veronikagovoruha@gmail.com>
2022-05-26 18:02:45 +0300

    add vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const onTimeUpdate = function (data) {
+  // _.throttle(() => {
+  //     console.log("Scroll handler call every 300ms");
+  //   }, 300)
+  localStorage.setItem('videoplayer-current-time', data['seconds']);
+};
+
+player.on('timeupdate', onTimeUpdate);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
