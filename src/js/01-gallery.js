b326fbf26c15823b3634367c512d159468961821
Veronika <veronikagovoruha@gmail.com>
2022-05-26 11:15:53 +0300

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,4 +4,31 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(SimpleLightbox);
+const galleryEl = document.querySelector('.gallery');
+galleryEl.addEventListener('click', openModal);
+
+function renderGalleryList(array) {
+  const listMarkup = array
+    .map(({ preview, original, description } = {}) => {
+      return `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+    })
+    .join('');
+  galleryEl.insertAdjacentHTML('beforeend', listMarkup);
+}
+
+renderGalleryList(galleryItems);
+
+function openModal(event) {
+  event.preventDefault();
+  const currentImg = event.target;
+  if (currentImg.nodeName !== 'IMG') {
+    return;
+  }
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
